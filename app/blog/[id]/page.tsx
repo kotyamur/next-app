2339d252bfaa5f7583e8dbfad92668721399bbe4
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-27 20:13:22 +0300

    add error, loading, data fetching

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
 
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Unable to fetch post");
+  }
+  return response.json();
+}
+
 type Props = {
   params: {
     id: string;
@@ -7,11 +22,19 @@ type Props = {
 };
 
 export async function generateMetadata({ params: { id } }: Props): Promise<Metadata> {
+    const post = await getData(id);
   return {
-    title: id,
+    title: post.title,
   };
 }
 
-export default function Post({ params: { id } }: Props) {
-  return <div>Post page{id}</div>;
+export default async function Post({ params: { id } }: Props) {
+    const post = await getData(id);
+    return (
+      <>
+        <h1>Post page{id}</h1>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </>
+    );
 }
